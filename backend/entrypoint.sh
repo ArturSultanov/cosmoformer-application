79448df1ce62e936200bb0a84ad7ac3d55cb9110
Artur Sultanov <sultanov.personal@gmail.com>
2025-04-02 21:51:00 +0200

    chore(): Improve dockerfile for rootless podman compatibility

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,6 @@ if [ -z "$WEB_CONCURRENCY" ]; then
   export WEB_CONCURRENCY=$(nproc)
 fi
 
-echo "Number of workers: $WEB_CONCURRENCY"
+echo "INFO:     Number of workers: $WEB_CONCURRENCY"
 
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --workers "$WEB_CONCURRENCY"
